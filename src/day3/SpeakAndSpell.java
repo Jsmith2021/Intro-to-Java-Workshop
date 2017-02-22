e578f05d86db654536f83282b9cbe6a0e3443745
Jsmith2021 <league@WTS-iMac-11.attlocal.net>
2017-02-22 11:50:38 -0800

    Workshop Day 3

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -14,17 +14,37 @@ import javax.swing.JOptionPane;
 public class SpeakAndSpell {
 
 	public static void main(String[] args) {
+			
+		
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+speak ("spell elephant");
 		// 2. Catch the user's answer in a String
-
+String word = JOptionPane.showInputDialog("Type the word in the space below");
 		// 3. If the user spelled the word correctly, speak "correct"
-
+if(word.equals("elephant")){
+	JOptionPane.showConfirmDialog(null, "That is correct");
+}
 		// 4. Otherwise say "wrong"
+else{
+	JOptionPane.showConfirmDialog(null, "That is incorrect");}
+
+
 
 		// 5. repeat the process for other words
-		
-	}
+	
+	
+	speak("spell sentimental");
+	
+ word = JOptionPane.showInputDialog("Type the word in the space below");
+	
+if(word.equals("sentimental")){
+JOptionPane.showConfirmDialog(null, "That is correct");
+}
+	
+else{
+JOptionPane.showConfirmDialog(null, "That is incorrect");}
+}
+
 
 	static void speak(String words) {
 		try {
